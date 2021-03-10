efad001f975c6fc935b0fa3067fb9f85a07381bf
minamikanesawa <63468191+minamikanesawa@users.noreply.github.com>
2021-03-10 10:55:46 +0900

    updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishablekey = 'pk_test_51HAKlyHZzr6vmedyfXVZX4mpMMveRP82h4QEGwar3LrBP9yJvUx9ffFNOtBfJDhDOx34XGzMvakoG4LaZMJ850hM00DsClGkeZ';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='eshop'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishablekey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
